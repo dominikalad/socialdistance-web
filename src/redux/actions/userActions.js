03d10c6dbefb093be1c42779ecfe95e85094abd9
Dominika <dominika.ladziak@gmail.com>
2021-01-15 18:33:38 -0800

    Profile info card

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -4,6 +4,7 @@ import {
   CLEAR_ERRORS,
   LOADING_UI,
   SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from "../types";
 import axios from "axios";
 
@@ -52,6 +53,7 @@ export const logoutUser = () => (dispatch) => {
 };
 
 export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
   axios
     .get("/user")
     .then((res) => {
